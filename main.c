63428a375e07a305eda20592e839655f7a27d167
Epiano28K <nickwu0301@gmail.com>
2018-07-11 12:00:25 +0800

    Time limit still exceeded.(Only search 5)

--- main.c
+++ main.c
@@ -1,58 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int min(int num1, int num2)
-{
-    if (num1 > num2)
-    {
-        return num2;
-    }
-    else if (num1 < num2)
-    {
-        return num1;
-    }
-    return num1;
-}
-
-void find2and5(int num, int* cntTwo, int* cntFive)
-{
-    int numTemp = num;
-    int cnt2, cnt5;
-
-    cnt2 = *cntTwo;
-    cnt5 = *cntFive;
-
-    while(numTemp % 2 == 0)
-    {
-        cnt2++;
-        numTemp /= 2;
-    }
-
-    numTemp = num;
-    while(numTemp % 5 == 0)
-    {
-        cnt5++;
-        numTemp /= 5;
-    }
-    *cntTwo  = cnt2;
-    *cntFive = cnt5;
-    //printf("%d.%d\n", *cntTwo, *cntFive);
-}
-
 int trailingZeroes(int n)
 {
-    int zeroes  = 0;
-    int cntTwo, cntFive;
+    int cntFive;
+    int numTemp;
 
-    cntTwo = cntFive = 0;
+    cntFive = 0;
 
     for (n; n > 0; n--)
     {
-        find2and5(n, &cntTwo, &cntFive);
+        numTemp = n;
+        while(numTemp % 5 == 0)
+        {
+            cntFive++;
+            numTemp /= 5;
+        }
     }
-    printf("Cnt2.5|%d.%d\n", cntTwo, cntFive);
-    zeroes = min(cntTwo, cntFive);
-    return zeroes;
+    return cntFive;
 }
 
 int main(void)
