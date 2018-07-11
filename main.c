d46021bef8763528f1c320f939c07bed04efbb20
Epiano28K <nickwu0301@gmail.com>
2018-07-11 23:15:49 +0800

    [EASY] 13. Roman to Integer

--- main.c
+++ main.c
@@ -1,12 +1,71 @@
 #include <stdio.h>
 
-int function()
+int romanToInt(char* s)
 {
+    int  iCnt, answer;
+
+    iCnt = answer = 0;
+    while(s[iCnt] != '\0')
+    {
+        switch (s[iCnt])
+        {
+            case 'I':
+                if ((s[iCnt+1] == 'V') || (s[iCnt+1] == 'X'))
+                {
+                    answer -= 1;
+                }
+                else
+                {
+                    answer += 1;
+                }
+                break;
+            case 'V':
+                answer += 5;
+                break;
+            case 'X':
+                if ((s[iCnt+1] == 'L') || (s[iCnt+1] == 'C'))
+                {
+                    answer -= 10;
+                }
+                else
+                {
+                    answer += 10;
+                }
+                break;
+            case 'L':
+                answer += 50;
+                break;
+            case 'C':
+                if ((s[iCnt+1] == 'D') || (s[iCnt+1] == 'M'))
+                {
+                    answer -= 100;
+                }
+                else
+                {
+                    answer += 100;
+                }
+                break;
+            case 'D':
+                answer += 500;
+                break;
+            case 'M':
+                answer += 1000;
+                break;
+            default:
+                break;
+        }
+        iCnt++;
+    }
+    return answer;
 }
 
 int main(void)
 {
+    char* roman = {"MCMXCIV"};
+    int rom2Int;
 
+    rom2Int = romanToInt(roman);
+    printf("%s|%d\n", roman, rom2Int);
     getchar();
     return 0;
 }
