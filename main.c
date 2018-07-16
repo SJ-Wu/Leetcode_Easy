c1858a740eb855c32d2a6ef91aa04f69ed4be820
Epiano28K <nickwu0301@gmail.com>
2018-07-16 22:29:04 +0800

    [EASY] #88 Merge Sorted Array.

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
-int function()
+void merge(int* nums1, int m, int* nums2, int n)
 {
+    int cntM, cntN, cntL;
+    int numTemp;
+    for (cntN = 0; cntN < n; cntN++)
+    {
+        for (cntM = 0; cntM < (m + cntN) ; cntM++)
+        {
+            if (nums2[cntN] < nums1[cntM])
+            {
+                numTemp     = nums2[cntN];
+                nums2[cntN] = nums1[cntM];
+                nums1[cntM] = numTemp;
+            }
+        }
+        nums1[cntM] = nums2[cntN];
+    }
 }
 
 int main(void)
 {
-
+    int nums1[9] = {-1,0,0,3,3,3,0,0,0};
+    int nums2[3] = {1,2,2};
+    merge(nums1, 6, nums2, 3);
+    for (int i = 0; i < 9; i++)
+    {
+        printf("%d.", nums1[i]);
+    }
     getchar();
     return 0;
 }
