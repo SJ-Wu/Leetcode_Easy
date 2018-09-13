06c8c4e821a82ce174e6e5cbe8ed490cc38d71a7
Epiano28K <nickwu0301@gmail.com>
2018-09-14 00:12:29 +0800

    Reference leetcode

--- main.c
+++ main.c
@@ -16,46 +16,47 @@ MyLinkedList* myLinkedListCreate() {
     return obj;
 }
 
+MyLinkedList* getNode(MyLinkedList* obj, int index){
+    MyLinkedList* cur = obj;
+    for(int i = 0; i < index && cur; i++){
+        cur = cur->next;
+    }
+    return cur;
+}
+
+MyLinkedList* getTail(MyLinkedList* obj){
+    MyLinkedList* cur = obj;
+    while(cur && cur->next){
+        cur = cur->next;
+    }
+    return cur;
+}
+
 /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
 int myLinkedListGet(MyLinkedList* obj, int index) {
-    MyLinkedList* node = obj;
-    if (index < 0) {
-        return -1;
-    }
-    else {
-        for(int cnt = 0; cnt < index; cnt++) {
-            if (node->next == NULL) return -1;
-            node = node->next;
-        }
-        return node->val;
-    }
+    MyLinkedList* cur = getNode(obj, index);
+    return cur == NULL ? -1 : cur->val;
 }
 
 /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
 void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
-    int tmpVal;
-    MyLinkedList* node = myLinkedListCreate();
-    if (obj->val != 0){
-        tmpVal    = obj->val;
-        obj->val  = val;
-        node->val = tmpVal;
-        node->next = obj->next;
-        obj->next  = node;
-    }
-    else{
-        obj->val = val;
-    }
+    MyLinkedList* cur = myLinkedListCreate();
+    cur->val = val;
+    cur->next = obj;
+    obj = cur;
+    return;
 }
 
 /** Append a node of value val to the last element of the linked list. */
 void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
-    MyLinkedList* tail = obj;
-    MyLinkedList* node = myLinkedListCreate();
-    node->val = val;
-    while(tail->next != NULL) {
-        tail = tail->next;
+    if (obj == NULL){
+        myLinkedListAddAtHead(obj, val);
+        return;
     }
-    tail->next = node;
+    MyLinkedList* prev = getTail(obj);
+    MyLinkedList* cur  = myLinkedListCreate();
+    cur->val = val;
+    prev->next = cur;
 }
 
 /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
@@ -65,37 +66,31 @@ void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
     newNode->val = val;
     if (index == 0){
         myLinkedListAddAtHead(obj, val);
-    }else {
-        for(int cnt = 1; cnt < index; cnt++) {
-            if (node->next == NULL) return;// The index is greater than the length.
-            node = node->next;
-        }
-        if(node->next == NULL){
-            myLinkedListAddAtTail(obj, val);
-        }
-        else {
-            newNode->next = node->next;
-            node->next    = newNode;
-        }
+        return;
     }
+    MyLinkedList* prev = getNode(obj, index-1);
+    if (prev == NULL) {
+        return;
+    }
+    MyLinkedList* cur = myLinkedListCreate();
+    cur->val = val;
+    MyLinkedList* next = prev->next;
+    cur->next = next;
+    prev->next = cur;
 }
 
 /** Delete the index-th node in the linked list, if the index is valid. */
 void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
-    MyLinkedList* node = obj;
-    if (index == 0){
-        obj = NULL;
-    }else {
-        for(int cnt = 1; cnt < index; cnt++) {
-            if (node->next == NULL) return;// The index is greater than the length.
-            node = node->next;
-        }
-        if(node->next == NULL){
-            node = NULL;
-        }
-        else {
-            node->next = node->next->next;
-        }
+    MyLinkedList* cur = getNode(obj, index);
+    if (cur == NULL){
+        return;
+    }
+    MyLinkedList* prev = getNode(obj, index-1);
+    MyLinkedList* next = cur->next;
+    if (prev) {
+        prev->next = next;
+    } else{
+        obj = next;
     }
 }
 
